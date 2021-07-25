5f6f8211703ee24ec300e841ea45994f43361cfd
luhui <luhuins@163.com>
2021-07-25 09:46:23 +0800

    New World

---
+++ haunt.scm
@@ -0,0 +1,16 @@
+(use-modules (haunt asset)
+	     (haunt builder blog)
+	     (haunt builder atom)
+	     (haunt reader)
+	     (haunt site))
+
+(site #:title "Lu Hui 's BLOG"
+      #:posts-directory "posts/"
+      #:domain "luhuins.github.io"
+      #:default-metadata
+      '((author . "Lu Hui")
+	(email . "luhux@foxmail.com"))
+      #:readers (list html-reader)
+      #:builders (list (blog)
+		       (atom-feed)
+		       (atom-feeds-by-tag)))
